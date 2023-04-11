c0f9968562d33c42e3473b22d5989e88a4bbbb46
Mayank Raj <mayankrj1224@gmail.com>
2023-04-11 20:15:09 +0530

    completd

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const UserRouter = require('./routes/userLogin');
 const DataRouter = require('./routes/UserData');
 const app = express();
-const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/', UserRouter);
@@ -19,6 +18,6 @@ connect().then(() => {
 }).catch((err) => {
     console.log(err.message);
 })
-app.listen(port, () => {
-    console.log(`server listening at http://localhost:${port}`);
+app.listen(8080, () => {
+    console.log(`server listening at http://localhost:8080`);
 })
